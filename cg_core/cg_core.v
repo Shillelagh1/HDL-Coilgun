dd83fc7be37e204080205d98f3c9e9b8a38d88d4
Shillelagh <sshillelagh@gmail.com>
2025-06-25 20:04:28 -0500

    Reorganization. Fixes a bug relating to RTE when LEN is unset. Fixes the edge detection for trigger and gate.

--- cg_core/cg_core.v
+++ cg_core/cg_core.v
@@ -23,60 +23,44 @@ output[23:0] O_ACC
     reg R_CD = 0;           // Count Direction Register
     reg R_CDIV = 0;         // Clock divider
     
-    reg R_I_TRIG = 0;       // Trigger edge detection register
+    reg R_I_TRIG = 0;       // Trigger edge detection register (Previous Value)
     reg R_I_GATE = 0;       // Gate edge detection register
     reg R_I_RST = 0;        // Reset edge detection register
 
     assign O_EXT = !R_SOE & R_CD & I_EN;
     assign O_SOE = R_SOE & R_CD & I_OE & I_EN;
-    assign O_RTE = R_ACC >= I_LMT;
+    assign O_RTE = (R_ACC >= I_LMT) & R_CD & I_LEN;
     assign O_ACC = R_ACC;
-
-    // Trigger input
-    always @(posedge I_TRIG) begin
-        R_I_TRIG <= 1;
-    end
-
-    // Gate input
-    always @(posedge I_GATE) begin
-        R_I_GATE <= 1;
-    end
-
-    // Reset input
-    always @(posedge I_RST) begin
-        R_I_RST <= 1;
-    end
 
     always @(posedge clk) begin
         R_CDIV <= !R_CDIV;
 
+        // Edge detection register
+        R_I_RST <= I_RST;
+        R_I_TRIG <= I_TRIG;
+        R_I_GATE <= I_GATE;
+
         // Reset functionality
-        if (R_I_RST) begin
+        if ((!R_I_RST && I_RST)) begin
             R_SOE <= 0;
             R_CD <= 0;
             R_CDIV <= 0;
             R_ACC <= 0;
             R_I_TRIG <= 0;
             R_I_GATE <= 0;
-
-            R_I_RST <= 0;
         end
 
         // Trigger functionality
-        if (R_I_TRIG & I_EN & !R_SOE) begin
+        if ((!R_I_TRIG && I_TRIG) & I_EN & !R_SOE) begin
             R_SOE <= 1;
             R_CD <= 0;
             R_CDIV <= 0;
             R_ACC <= I_DLY;
-
-            R_I_TRIG <= 0;
         end
 
         // Gate functionality
-        if (R_I_GATE & R_SOE) begin
+        if ((!R_I_GATE && I_GATE) & R_SOE) begin
             R_SOE <= 0;
-
-            R_I_GATE <= 0;
         end
 
         // Counter Functionality
